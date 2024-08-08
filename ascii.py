f6e878a48c940077612901324c80811aecd374ee
Cr1m <o_devon2@yahoo.com>
2024-08-08 14:03:44 -0400

    Changed the ascii art to allow for the changing of colors

--- ascii.py
+++ ascii.py
@@ -1,45 +1,33 @@
-card_art = [
- '''
- _____ 
-|A .  |
-| /.\ |
-|(_._)|
-|  |  |
-|____V|
-       
-''','''
- _____ 
-|A ^  |
-| / \ |
-| \ / |
-|  .  |
-|____V|
-       
-''','''
- _____ 
-|A _  | 
-| ( ) |
-|(_'_)|
-|  |  |
-|____V|
-       
-''','''
- _____ 
-|A_ _ |
-|( v )|
-| \ / |
-|  .  |
-|____V|
-       
-'''
-          
-          
-         
-         
-]
+import systemColors
 
-
-
-
-
+card_art = [[
+systemColors.BLACK + " _____ ",
+systemColors.BLACK + "|A .  |",
+systemColors.BLACK + "| /.\ |",
+systemColors.BLACK + "|(_._)|",
+systemColors.BLACK + "|  |  |",
+systemColors.BLACK + "|____V|"
+],[
+systemColors.RED + " _____ ",
+systemColors.RED + "|A ^  |",
+systemColors.RED + "| / \ |",
+systemColors.RED + "| \ / |",
+systemColors.RED + "|  .  |",
+systemColors.RED + "|____V|"
+],
+[
+systemColors.BLACK + " _____ ",
+systemColors.BLACK + "|A _  |",
+systemColors.BLACK + "| ( ) |",
+systemColors.BLACK + "|(_'_)|",
+systemColors.BLACK + "|  |  |",
+systemColors.BLACK + "|____V|"
+],[
+systemColors.RED + " _____ ",
+systemColors.RED + "|A_ _ |",
+systemColors.RED + "|( v )|",
+systemColors.RED + "| \ / |",
+systemColors.RED + "|  .  |",
+systemColors.RED + "|____V|"
+]]
 
