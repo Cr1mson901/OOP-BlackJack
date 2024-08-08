d289ae23364c30a84df7a6695150a9a6940b6cd5
Cr1m <o_devon2@yahoo.com>
2024-08-08 15:27:23 -0400

    added color scheme function

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from platform   import system as system_name  # Returns the system/OS name
 from subprocess import call   as system_call  # Execute a shell command
+import systemColors
 
 def clear_screen():
     # Clear screen command as function of OS
@@ -7,3 +8,7 @@ def clear_screen():
 
     # Action
     system_call([command])
+
+def color_scheme(background = systemColors.WHITEB):
+    #Sets the terminal to print in bold with a colored background
+    print(f"{systemColors.B}{background}")
