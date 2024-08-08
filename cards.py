44d6fd9ebef4a26ca708a59f37ce5fc332ee52d1
Cr1m <o_devon2@yahoo.com>
2024-08-08 14:17:53 -0400

    Moved print function to new class hand

--- cards.py
+++ cards.py
@@ -1,3 +1,12 @@
 class deck:
     def __init__(self) -> None:
         pass
+
+class hand:
+    spacing = " " * 2
+    def __init__(self) -> None:
+        pass
+
+    def print(self):
+        for pieces in zip(*self):
+            print(self.spacing.join(pieces))
