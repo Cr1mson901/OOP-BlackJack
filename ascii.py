10b8edc0af2220711df850d401fe77ce76cf5949
Cr1m <o_devon2@yahoo.com>
2024-08-08 15:01:07 -0400

    Added number card ascii art

--- ascii.py
+++ ascii.py
@@ -1,6 +1,6 @@
 import systemColors
 
-card_art = [[
+aces = [[
 systemColors.BLACK + " _____ ",
 systemColors.BLACK + "|A .  |",
 systemColors.BLACK + "| /.\ |",
@@ -31,3 +31,68 @@ systemColors.RED + "|  .  |",
 systemColors.RED + "|____V|"
 ]]
 
+numbers = {
+2:[
+systemColors.RED + " _____ ",
+systemColors.RED + "|2    |",
+systemColors.RED + "|  ♡  |",
+systemColors.RED + "|     |",
+systemColors.RED + "|  ♡  |",
+systemColors.RED + "|____2|"
+],3:[
+systemColors.RED + " _____ ",
+systemColors.RED + "|3    |",
+systemColors.RED + "|  ♡  |",
+systemColors.RED + "|  ♡  |",
+systemColors.RED + "|  ♡  |",
+systemColors.RED + "|____3|"
+],4:[
+systemColors.RED + " _____ ",
+systemColors.RED + "|4    |",
+systemColors.RED + "| ♡ ♡ |",
+systemColors.RED + "|     |",
+systemColors.RED + "| ♡ ♡ |",
+systemColors.RED + "|____4|"
+],5:[
+systemColors.RED + " _____ ",
+systemColors.RED + "|5    |",
+systemColors.RED + "| ♡ ♡ |",
+systemColors.RED + "|  ♡  |",
+systemColors.RED + "| ♡ ♡ |",
+systemColors.RED + "|____5|"
+],6:[
+systemColors.RED + " _____ ",
+systemColors.RED + "|6    |",
+systemColors.RED + "| ♡ ♡ |",
+systemColors.RED + "| ♡ ♡ |",
+systemColors.RED + "| ♡ ♡ |",
+systemColors.RED + "|____6|"
+],7:[
+systemColors.RED + " _____ ",
+systemColors.RED + "|7♡ ♡ |",
+systemColors.RED + "|  ♡  |",
+systemColors.RED + "| ♡ ♡ |",
+systemColors.RED + "| ♡ ♡ |",
+systemColors.RED + "|____7|"
+],8:[
+systemColors.RED + " _____ ",
+systemColors.RED + "|8♡ ♡ |",
+systemColors.RED + "| ♡ ♡ |",
+systemColors.RED + "| ♡ ♡ |",
+systemColors.RED + "| ♡ ♡ |",
+systemColors.RED + "|____8|"
+],9:[
+systemColors.RED + " _____ ",
+systemColors.RED + "|9    |",
+systemColors.RED + "|♡ ♡ ♡|",
+systemColors.RED + "|♡ ♡ ♡|",
+systemColors.RED + "|♡ ♡ ♡|",
+systemColors.RED + "|____9|"
+],10:[
+systemColors.RED + " _____ ",
+systemColors.RED + "|10♡ ♡|",
+systemColors.RED + "|♡ ♡ ♡|",
+systemColors.RED + "|♡ ♡ ♡|",
+systemColors.RED + "|♡ ♡  |",
+systemColors.RED + "|___10|"
+]}
