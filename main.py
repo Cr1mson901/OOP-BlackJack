bb5f4ee56cce70e9e783f4aeba077de01f3f3d79
Cr1m <o_devon2@yahoo.com>
2024-08-08 15:02:03 -0400

    Suits are added and concept for printing cards

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import cards
-from ascii import card_art #Art for the cards
+from ascii import aces, numbers #Art for the cards
 import systemColors #A way to color outputs in the consol
 from platform   import system as system_name  # Returns the system/OS name
 from subprocess import call   as system_call  # Execute a shell command
 
-spacing = ' ' * 2  # Between cards.
 background = systemColors.WHITEB
 card_color = systemColors.RED
-print(f"{systemColors.B}")
+print(f"{systemColors.B}{background}")
 
-
+suits = {
+    "heart":  ("♡",systemColors.RED),
+    "diamond":("⬦",systemColors.RED),
+    "spade":  ("♤",systemColors.BLACK),
+    "club":   ("♧",systemColors.BLACK)
+}
 
 def clear_screen():
     # Clear screen command as function of OS
@@ -20,11 +24,7 @@ def clear_screen():
 
 clear_screen()
 
-cards = card_art[0], card_art[3], card_art[1]
-
-print(f"{card_color}{background}")
-for pieces in zip(*(card.splitlines() for card in cards)):
-    print(spacing.join(pieces))
-
-
-
+# #Prints cards in cards
+cards = [[line.replace("♡","⬦") for line in numbers[6]],[line.replace("♡","♤").replace(systemColors.RED, systemColors.BLACK) for line in numbers[6]],numbers[10]]
+for pieces in zip(*cards):
+    print("  ".join(pieces))
