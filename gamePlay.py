70e4c04b02d9f588db2644dcaf971a8cb1e6d06a
Cr1m <o_devon2@yahoo.com>
2024-08-12 16:07:53 -0400

    Relocated total to cards file

--- gamePlay.py
+++ gamePlay.py
@@ -10,23 +10,3 @@ def bankroll_gen(name):
         gui.clear_screen()
     print(f"{name} you will be starting with ${bankroll}")
     return bankroll
-
-def total(cards):
-    aces = []
-    numbers = []
-    for card in cards:
-        if card[0] == 1:
-            aces.append(11)
-        elif card[0] == 14:
-            continue
-        elif card[0] > 9:
-            numbers.append(10)
-        else:
-            numbers.append(card[0])
-    count = sum(numbers)
-    while aces:
-        if count + aces.pop() + len(aces) < 22:
-            count += 11
-        else:
-            count += 1
-    return count
