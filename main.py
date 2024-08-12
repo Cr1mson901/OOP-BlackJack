d861ce04fa5242baba4e0fc80b86e65f2c129fa9
Cr1m <o_devon2@yahoo.com>
2024-08-12 15:03:08 -0400

    Added test cases and a randomizer for the bankroll

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import cards
 import gui
-from ascii import faces, numbers #Art for the cards
 import systemColors #A way to color outputs in the consol
+import random
+import time
+
+player_name = input("Hello player, what is your name?\n").capitalize()
+#Randomizes the starting bankroll, making it kinda feel like slot wheel
+bankroll = random.randint(500,5000)
+for i in range(50):
+    print(f"{player_name} you will be starting with ${random.randint(500,5000)}")
+    time.sleep(.025)
+    gui.clear_screen()
+print(f"{player_name} you will be starting with ${bankroll}")
 
 gui.color_scheme()
+#Used for troubleshooting
+player = cards.hand(player_name)
+dealer = cards.hand()
+shoe = cards.shoe()
+shoe.build(3)
+dealer.hit(shoe)
+dealer.hit(shoe)
+player.hit(shoe)
+player.hit(shoe)
+player.hand_print()
+dealer.dealer_print()
 
-# #Prints cards in cards
-hand = cards.hand()
-hand.cards = [[line.replace("♡","⬦") for line in numbers[6]],[line.replace("♡","♤").replace(systemColors.RED, systemColors.BLACK) for line in numbers[6]],numbers[10],faces["spade"]]
-hand.print()
+while input(f"{systemColors.RESET}Want to hit?")[0] != "n":
+    player.hit(shoe)
+    gui.clear_screen()
+    player.hand_print()
