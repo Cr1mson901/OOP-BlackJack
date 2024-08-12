6af6f2613d9b89c8a398f8609ea74a1de68c9a21
Cr1m <o_devon2@yahoo.com>
2024-08-12 13:56:01 -0400

    Updated cards to look much better

--- ascii.py
+++ ascii.py
@@ -2,132 +2,158 @@ import systemColors
 
 aces = {
 "spade":[
-systemColors.BLACK + " _____ ",
-systemColors.BLACK + "|A .  |",
-systemColors.BLACK + "| /.\ |",
-systemColors.BLACK + "|(_._)|",
-systemColors.BLACK + "|  |  |",
-systemColors.BLACK + "|____A|"
+systemColors.BLACK + "╔═════╗",
+systemColors.BLACK + "║A Λ  ║",
+systemColors.BLACK + "║ /.\\ ║",
+systemColors.BLACK + "║(_._)║",
+systemColors.BLACK + "║  ║  ║",
+systemColors.BLACK + "║    A║",
+systemColors.BLACK + "╚═════╝"
 ],
 "diamond":[
-systemColors.RED + " _____ ",
-systemColors.RED + "|A ^  |",
-systemColors.RED + "| / \ |",
-systemColors.RED + "| \ / |",
-systemColors.RED + "|  .  |",
-systemColors.RED + "|____A|"
+systemColors.RED + "╔═════╗",
+systemColors.RED + "║A Λ  ║",
+systemColors.RED + "║ / \\ ║",
+systemColors.RED + "║ \\ / ║",
+systemColors.RED + "║  V  ║",
+systemColors.RED + "║    A║",
+systemColors.RED + "╚═════╝"
 ],
 "club":[
-systemColors.BLACK + " _____ ",
-systemColors.BLACK + "|A _  |",
-systemColors.BLACK + "| ( ) |",
-systemColors.BLACK + "|(_'_)|",
-systemColors.BLACK + "|  |  |",
-systemColors.BLACK + "|____A|"
+systemColors.BLACK + "╔═════╗",
+systemColors.BLACK + "║A    ║",
+systemColors.BLACK + "║ (‾) ║",
+systemColors.BLACK + "║(_'_)║",
+systemColors.BLACK + "║  ║  ║",
+systemColors.BLACK + "║    A║",
+systemColors.BLACK + "╚═════╝"
 ],
 "heart":[
-systemColors.RED + " _____ ",
-systemColors.RED + "|A_ _ |",
-systemColors.RED + "|( v )|",
-systemColors.RED + "| \ / |",
-systemColors.RED + "|  .  |",
-systemColors.RED + "|____A|"
+systemColors.RED + "╔═════╗",
+systemColors.RED + "║A_ _ ║",
+systemColors.RED + "║( v )║",
+systemColors.RED + "║ \\ / ║",
+systemColors.RED + "║  V  ║",
+systemColors.RED + "║    A║",
+systemColors.RED + "╚═════╝"
 ]}
 
 numbers = {
 2:[
-systemColors.RED + " _____ ",
-systemColors.RED + "|2    |",
-systemColors.RED + "|  ♡  |",
-systemColors.RED + "|     |",
-systemColors.RED + "|  ♡  |",
-systemColors.RED + "|____2|"
+systemColors.RED + "╔═════╗",
+systemColors.RED + "║2    ║",
+systemColors.RED + "║  ♡  ║",
+systemColors.RED + "║     ║",
+systemColors.RED + "║  ♡  ║",
+systemColors.RED + "║    2║",
+systemColors.RED + "╚═════╝"
 ],
 3:[
-systemColors.RED + " _____ ",
-systemColors.RED + "|3    |",
-systemColors.RED + "|  ♡  |",
-systemColors.RED + "|  ♡  |",
-systemColors.RED + "|  ♡  |",
-systemColors.RED + "|____3|"
+systemColors.RED + "╔═════╗",
+systemColors.RED + "║3    ║",
+systemColors.RED + "║  ♡  ║",
+systemColors.RED + "║  ♡  ║",
+systemColors.RED + "║  ♡  ║",
+systemColors.RED + "║    3║",
+systemColors.RED + "╚═════╝"
 ],
 4:[
-systemColors.RED + " _____ ",
-systemColors.RED + "|4    |",
-systemColors.RED + "| ♡ ♡ |",
-systemColors.RED + "|     |",
-systemColors.RED + "| ♡ ♡ |",
-systemColors.RED + "|____4|"
+systemColors.RED + "╔═════╗",
+systemColors.RED + "║4    ║",
+systemColors.RED + "║ ♡ ♡ ║",
+systemColors.RED + "║     ║",
+systemColors.RED + "║ ♡ ♡ ║",
+systemColors.RED + "║    4║",
+systemColors.RED + "╚═════╝"
 ],
 5:[
-systemColors.RED + " _____ ",
-systemColors.RED + "|5    |",
-systemColors.RED + "| ♡ ♡ |",
-systemColors.RED + "|  ♡  |",
-systemColors.RED + "| ♡ ♡ |",
-systemColors.RED + "|____5|"
+systemColors.RED + "╔═════╗",
+systemColors.RED + "║5    ║",
+systemColors.RED + "║ ♡ ♡ ║",
+systemColors.RED + "║  ♡  ║",
+systemColors.RED + "║ ♡ ♡ ║",
+systemColors.RED + "║    5║",
+systemColors.RED + "╚═════╝"
 ],
 6:[
-systemColors.RED + " _____ ",
-systemColors.RED + "|6    |",
-systemColors.RED + "| ♡ ♡ |",
-systemColors.RED + "| ♡ ♡ |",
-systemColors.RED + "| ♡ ♡ |",
-systemColors.RED + "|____6|"
+systemColors.RED + "╔═════╗",
+systemColors.RED + "║6    ║",
+systemColors.RED + "║ ♡ ♡ ║",
+systemColors.RED + "║ ♡ ♡ ║",
+systemColors.RED + "║ ♡ ♡ ║",
+systemColors.RED + "║    6║",
+systemColors.RED + "╚═════╝"
 ],
 7:[
-systemColors.RED + " _____ ",
-systemColors.RED + "|7♡ ♡ |",
-systemColors.RED + "|  ♡  |",
-systemColors.RED + "| ♡ ♡ |",
-systemColors.RED + "| ♡ ♡ |",
-systemColors.RED + "|____7|"
+systemColors.RED + "╔═════╗",
+systemColors.RED + "║7    ║",
+systemColors.RED + "║ ♡ ♡ ║",
+systemColors.RED + "║♡ ♡ ♡║",
+systemColors.RED + "║ ♡ ♡ ║",
+systemColors.RED + "║    7║",
+systemColors.RED + "╚═════╝"
 ],
 8:[
-systemColors.RED + " _____ ",
-systemColors.RED + "|8♡ ♡ |",
-systemColors.RED + "| ♡ ♡ |",
-systemColors.RED + "| ♡ ♡ |",
-systemColors.RED + "| ♡ ♡ |",
-systemColors.RED + "|____8|"
+systemColors.RED + "╔═════╗",
+systemColors.RED + "║8    ║",
+systemColors.RED + "║♡ ♡ ♡║",
+systemColors.RED + "║ ♡ ♡ ║",
+systemColors.RED + "║♡ ♡ ♡║",
+systemColors.RED + "║    8║",
+systemColors.RED + "╚═════╝"
 ],
 9:[
-systemColors.RED + " _____ ",
-systemColors.RED + "|9    |",
-systemColors.RED + "|♡ ♡ ♡|",
-systemColors.RED + "|♡ ♡ ♡|",
-systemColors.RED + "|♡ ♡ ♡|",
-systemColors.RED + "|____9|"
+systemColors.RED + "╔═════╗",
+systemColors.RED + "║9    ║",
+systemColors.RED + "║♡ ♡ ♡║",
+systemColors.RED + "║♡ ♡ ♡║",
+systemColors.RED + "║♡ ♡ ♡║",
+systemColors.RED + "║    9║",
+systemColors.RED + "╚═════╝"
 ],
 10:[
-systemColors.RED + " _____ ",
-systemColors.RED + "|10♡ ♡|",
-systemColors.RED + "|♡ ♡ ♡|",
-systemColors.RED + "|♡ ♡ ♡|",
-systemColors.RED + "|♡ ♡  |",
-systemColors.RED + "|___10|"
+systemColors.RED + "╔═════╗",
+systemColors.RED + "║10   ║",
+systemColors.RED + "║♡ ♡ ♡║",
+systemColors.RED + "║♡ ♡ ♡║",
+systemColors.RED + "║♡ ♡ ♡║",
+systemColors.RED + "║  ♡10║",
+systemColors.RED + "╚═════╝"
 ],
 11:[
-systemColors.RED + " _____ ",
-systemColors.RED + "| ♡♡♡♡|",
-systemColors.RED + "|   ♡ |",
-systemColors.RED + "|♡  ♡ |",
-systemColors.RED + "| ♡♡  |",
-systemColors.RED + "|____J|"
+systemColors.RED + "╔═════╗",
+systemColors.RED + "║ ♡♡♡♡║",
+systemColors.RED + "║   ♡ ║",
+systemColors.RED + "║♡  ♡ ║",
+systemColors.RED + "║ ♡♡  ║",
+systemColors.RED + "║    J║",
+systemColors.RED + "╚═════╝"
 ],
 12:[
-systemColors.RED + " _____ ",
-systemColors.RED + "| ♡♡♡ |",
-systemColors.RED + "|♡   ♡|",
-systemColors.RED + "|♡ ♡ ♡|",
-systemColors.RED + "| ♡♡♡ |",
-systemColors.RED + "|____Q|"
+systemColors.RED + "╔═════╗",
+systemColors.RED + "║ ♡♡♡ ║",
+systemColors.RED + "║♡   ♡║",
+systemColors.RED + "║♡ ♡ ♡║",
+systemColors.RED + "║ ♡♡♡ ║",
+systemColors.RED + "║    Q║",
+systemColors.RED + "╚═════╝"
+
 ],
 13:[
-systemColors.RED + " _____ ",
-systemColors.RED + "|♡   ♡|",
-systemColors.RED + "|♡ ♡  |",
-systemColors.RED + "|♡♡   |",
-systemColors.RED + "|♡ ♡  |",
-systemColors.RED + "|♡___K|"
+systemColors.RED + "╔═════╗",
+systemColors.RED + "║♡   ♡║",
+systemColors.RED + "║♡ ♡  ║",
+systemColors.RED + "║♡♡   ║",
+systemColors.RED + "║♡ ♡  ║",
+systemColors.RED + "║♡   K║",
+systemColors.RED + "╚═════╝"
+
 ]}
+face_down = [
+systemColors.BLACK + "╔═════╗",
+systemColors.BLACK + "║▒▒▒▒▒║",
+systemColors.BLACK + "║▒▒▒▒▒║",
+systemColors.BLACK + "║▒▒▒▒▒║",
+systemColors.BLACK + "║▒▒▒▒▒║",
+systemColors.BLACK + "╚═════╝"
+]
