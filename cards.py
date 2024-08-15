4aeb007bad72e295e6fd34e5d773150c5ebc9a73
Cr1m <38981037+Cr1mson901@users.noreply.github.com>
2024-08-15 18:35:17 -0400

    Adder is now called after every hit so self.count is reliable

--- cards.py
+++ cards.py
@@ -42,12 +42,14 @@ class hand:
     #TODO: add hit functionality
     def hit(self,shoe) -> bool:
         self.cards.append(shoe.stack.pop())
+        self.adder()
     
-    def hand_print(self,my_hand=0) -> None:
+    def hand_print(self,my_hand=0,covered=True) -> None:
         #Nested line comprehension that takes the each line of the ascii art and converts it to the correct suit
         if not my_hand:
             my_hand = self.cards
-        self.name_tag(my_hand)
+            covered = False
+        self.name_tag(covered)
         cards = [[line.replace("♡",suits[card[1]][0]).replace(systemColors.RED, suits[card[1]][1])
                 for line in ascii.numbers[card[0]]] 
                 if card[0] != 1 else ascii.aces[card[1]] for card in my_hand]
@@ -58,22 +60,26 @@ class hand:
     def dealer_print(self) -> None:
         self.hand_print([self.cards[0],[14,"heart"]])
             
-    #Prints a name tag for the cards to make it easier for the player to distinguish their cards        
-    def name_tag(self,my_hand) -> None:
-        print(f"{systemColors.RESET}{systemColors.RED}{self.name} {self.adder(my_hand)}")
+    #Prints a name tag and count for the cards to make it easier for the player to distinguish their cards        
+    def name_tag(self,covered=False) -> None:
+        if covered:
+            total = self.cards[0][0]
+        else:
+            total = self.count
+        print(f"{systemColors.RESET}{systemColors.RED}{self.name} {total}")
         gui.color_scheme()
 
-    #TODO: add bust check funtionality
+    #Returns true if busted
     def busted(self):
         if self.count > 21:
             return True
         else:
             return False
     
-    def adder(self,cards):
+    def adder(self):
         aces = []
         numbers = []
-        for card in cards:
+        for card in self.cards:
             if card[0] == 1:
                 aces.append(11)
             elif card[0] == 14:
