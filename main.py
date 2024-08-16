fc37eeaa81c23f6e4d717a0cb291a3402a861f57
Cr1m <38981037+Cr1mson901@users.noreply.github.com>
2024-08-15 21:41:47 -0400

    Nested black jack check

--- main.py
+++ main.py
@@ -16,84 +16,69 @@ def start() -> None:
             raise Exception("Not enough decks")
     except:
         decks = 3
-    bankroll = gamePlay.bankroll_gen(player_name)
     #Generates two hands, one for the player and one for the Dealer
     player = cards.hand(player_name)
     dealer = cards.hand()
+    player.bankroll = gamePlay.bankroll_gen(player.name)    
     #Makes a shoe entity so it can be filled with cards
     shoe = cards.shoe()
     shoe.build(decks)
-
-
 
 def deal_cards():
     player.hit(shoe)
     dealer.hit(shoe)
 
-#Acquires a valid bet from the player
-def get_bet(getting_bet=True):
-    while getting_bet:
-        global bet
-        try:
-            print(f"{player_name}, you have ${bankroll}")
-            bet = int(input(f"{systemColors.RESET}How much would you like to bet?\n"))
-            if bet > bankroll:
-                raise Exception("Not enough money")
-            elif bet < 1:
-                raise Exception("Not big enough of a bet")
-            else:
-                getting_bet = False
-        except:
-            gui.clear_screen()
-            print("Please input a valid bet")
-
 if __name__ == "__main__":
     start()
-    while bankroll > 0:
+    while player.bankroll > 0:
         #Resets hands
         dealer.cards = []
         player.cards = []
-        get_bet()
-
+        
+        #Gets bet
+        gamePlay.get_bet(player)
         #Deals out two cards to the player and dealer
         for i in range(2):
             deal_cards()
 
-        #Checks if Player or Dealer has a BlackJack before playing TODO: skip rest if blackjack
-        if player.count == 21:
-            print("Black Jack")
-            bankroll += int(bet * 1.5)
-        elif dealer.count == 21:
-            print("You lose")
-            bankroll -= bet
-      
-        #Displays the dealt out cards
-        gui.covered_print(True,dealer,player)
-        
-        while input(f"{systemColors.RESET}Want to hit?")[0] != "n":
-            player.hit(shoe)
+        #Checks if Player or Dealer has a BlackJack before playing
+        if player.count != 21 and dealer.count != 21:
+            #Displays the dealt out cards
             gui.covered_print(True,dealer,player)
-            #Stops if hand busts
-            if player.busted():
-                break
-
-        gui.covered_print(False,dealer,player)
-        time.sleep(1)
-        #Dealers turn if the player is still available
-        if not player.busted():
-            while dealer.count < 16:
-                print(systemColors.RESET,end="")
-                dealer.hit(shoe)
-                gui.covered_print(False,dealer,player)
-                time.sleep(1)
-                if dealer.busted():
+            
+            while input(f"{systemColors.RESET}Want to hit?")[0] != "n":
+                player.hit(shoe)
+                gui.covered_print(True,dealer,player)
+                #Stops if hand busts
+                if player.busted():
                     break
-            #TODO: fix this so ties work
-            print_max = lambda hand : print(f"{systemColors.RESET}{hand.name} is the winner of this hand with {hand.count}")
-            print_max(max(dealer,player,key=lambda hand : hand.count))
+
+            gui.covered_print(False,dealer,player)
+            time.sleep(1)
+            #Dealers turn if the player is still available
+            if not player.busted():
+                while dealer.count < 16:
+                    print(systemColors.RESET,end="")
+                    dealer.hit(shoe)
+                    gui.covered_print(False,dealer,player)
+                    time.sleep(1)
+                    if dealer.busted():
+                        break
+                #TODO: fix this so ties work
+                print_max = lambda hand : print(f"{systemColors.RESET}{hand.name} is the winner of this hand with {hand.count}")
+                print_max(max(dealer,player,key=lambda hand : hand.count))
+            else:
+                #Print busted message
+                print(f"{systemColors.RESET}{player.name} busted so the Dealer wins this round")
+                player.bankroll -= player.bet
+        elif player.count == 21 and dealer.count == 21:
+            print("Its a tie")
+            continue
+        elif player.count == 21:
+            print("Black Jack")
+            player.bankroll += int(player.bet * 1.5)
         else:
-            #Print busted message
-            print(f"{systemColors.RESET}{player.name} busted so the Dealer wins this round")
-            bankroll -= bet
+            print("You lose")
+            player.bankroll -= player.bet
         shoe.rebuild_check()
 print(systemColors.RESET,end="")
