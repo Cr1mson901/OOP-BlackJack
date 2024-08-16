e0a4cb9ebf31ed8eade81a939764b628017de5b9
Cr1m <38981037+Cr1mson901@users.noreply.github.com>
2024-08-15 21:41:18 -0400

    moved bet to gamePlay

--- gamePlay.py
+++ gamePlay.py
@@ -1,7 +1,9 @@
 import random
 import time
 import gui
+import systemColors
 #Randomizes the starting bankroll, making it kinda feel like slot wheel
+
 def bankroll_gen(name):
     bankroll = random.randint(500,5000)
     for i in range(50):
@@ -9,3 +11,21 @@ def bankroll_gen(name):
         time.sleep(.025)
         gui.clear_screen()
     return bankroll
+
+#Acquires a valid bet from the player
+def get_bet(player,getting_bet=True):
+    while getting_bet:
+        global bet
+        try:
+            print(f"{player.name}, you have ${player.bankroll}")
+            bet = int(input(f"{systemColors.RESET}How much would you like to bet?\n"))
+            if bet > player.bankroll:
+                raise Exception("Not enough money")
+            elif bet < 1:
+                raise Exception("Not big enough of a bet")
+            else:
+                getting_bet = False
+        except:
+            gui.clear_screen()
+            print("Please input a valid bet")
+    player.bet = bet
